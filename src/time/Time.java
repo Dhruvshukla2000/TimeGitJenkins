f2b193e90083783b2a86d4ada13da83631a76ce5
Dhruvshukla2000 <65990748+Dhruvshukla2000@users.noreply.github.com>
2022-03-10 13:31:13 -0500

    Github connection

--- src/time/Time.java
+++ src/time/Time.java
@@ -24,8 +24,9 @@ public class Time
 	public static void main(String[] args) 
 	{
 
-	int totalSeconds = getTotalSeconds("10:10:10");
-	System.out.println("Total Seconds=" + totalSeconds);
+		 int totalSeconds = getTotalSeconds("Github 10:10:10");
+		 System.out.println("Total Seconds = "+totalSeconds);
+
 	}
 //	public static void main(String[] args) 
 //	{
